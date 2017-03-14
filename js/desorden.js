26a57158f3764c32c98830b4abf6f156cf1c2666
Juve-yescas <juvenal@mail.com>
2017-03-14 16:47:21 -0600

    Add ip and mask 1st 2nd

--- js/desorden.js
+++ js/desorden.js
@@ -1,24 +1,24 @@
 var final_primera=true;
 var ips_calculadas=[];
 
-function revueltisimo() {
-	console.log("%cHola lola, laaa! laraaraa! looo! lalala! ","color:blue");
-}
-
 function main_revuelto() {
 	var asi_de_ips=Number(capturar_cuantas_ips());
 	console.log("---------------------------N0. IPS: ",asi_de_ips,"-------------------------")
 	var pot_resultado=elevar(asi_de_ips); //array de elevado y resultado
+	que_rango_mascara(Number(pot_resultado.potencia));
 	ip_inicial_revuelto();
 	ip_final_revuelto(pot_resultado.resultado_potencia);
-	que_rango_mascara(Number(pot_resultado.potencia));
 }
 
 function ip_inicial_revuelto() {
 	if (primera_vez) {
 		console.log("%cESTE ES VALIDO","color:orange");
 		primera_vez=false;
-		console.log("%c"+(ip_red.toString()).replace(/,/g,"."),"color:green");
+		imprimir("Ip inicial ",ip_red,mascara,"green");
+		ip_red[3]=ip_red[3]+1;
+		imprimir("1st valida: ",ip_red,mascara,"blue");
+		ip_red[3]=ip_red[3]+1;
+		imprimir("2nd valida: ",ip_red,mascara,"blue");
 		/*var arr = [ip_red,0]; //Analisis vectorial función
 		ips_calculadas.push(arr);*/
 	}
@@ -30,10 +30,10 @@ function ip_final_revuelto(elmultiplo) {
 		calcula_ip_final(elmultiplo);
 	}else{
 		if (elmultiplo<=256) {
-			console.log("%cIncrementa ip en: 0.0.0."+(elmultiplo-1),"color:green"); 
+			imprimir("Incrementa ip en ",("0.0.0."+(elmultiplo-1)),mascara,"green");
 		}else{
 			var mod_this=rango_modificar(elmultiplo);
-			console.log("%cIncrementa ip en: 0.0."+mod_this+".255","color:green")
+			imprimir("Incrementa ip en ",("0.0.0."+(mod_this)+".255"),mascara,"green");
 		}	
 	}
 }
--- js/normal.js
+++ js/normal.js
@@ -1,11 +1,12 @@
 var ip_red;
 var normal=true;
 var primera_vez=true;
+var mascara="";
 
 function main() {
 	console.clear();
 	if (document.getElementById('normal').checked != true) {
-		normal=false; revueltisimo(); console.log("Manual, trabajando para automatico ;)");
+		normal=false; console.log("Manual, trabajando para automatico ;)");
 	}
 	ip_red = document.getElementById("ip_red").value;
 	ip_red= ip_red.split(".").map(Number);
@@ -16,9 +17,10 @@ function principal() {
 	var asi_de_ips=Number(capturar_cuantas_ips());
 	console.log("---------------------------N0. IPS: ",asi_de_ips,"-------------------------")
 	var pot_resultado=elevar(asi_de_ips);
+	que_rango_mascara(Number(pot_resultado.potencia));
 	calcula_ip_inicial();
 	calcula_ip_final(pot_resultado.resultado_potencia);
-	que_rango_mascara(Number(pot_resultado.potencia));
+	
 }
 
 function calcula_ip_inicial() {
@@ -30,13 +32,24 @@ function calcula_ip_inicial() {
 			ip_red[2]=ip_red[2]+1; ip_red[3]=0;
 		}
 	}
-	console.log("%cIp inicial: "+(ip_red.toString()).replace(/,/g,"."),"color:green");
+	imprimir("ip inicial: ",ip_red,mascara,"green");
+	var ipmod=ip_red.slice();;
+	ipmod[3]=ipmod[3]+1;
+	imprimir("1st valida: ",ipmod,mascara,"blue");
+	ipmod[3]=ipmod[3]+1;
+	imprimir("2nd valida: ",ipmod,mascara,"blue");
+}
+
+function imprimir(tipoip,mip,maskara,color) {
+	console.log("%c"+tipoip+(mip.toString()).replace(/,/g,".")+" "+maskara,"color:"+color);
 }
 
 function calcula_ip_final(elmultiplo){
 	if (elmultiplo<=256) {
 		ip_red[3]=ip_red[3]+ (elmultiplo-1); 
-		if (ip_red[3]>255) {console.log("%cAlgo anda mal, hacerlo manual, o pregunta a Manuel","color:red")}
+			if (ip_red[3]>255) {
+				console.log("%cAlgo anda mal, hacerlo manual, o pregunta a Manuel","color:red")
+			}
 	}else{
 		if (ip_red[2]<255) {
 			ip_red[2]=ip_red[2]+(rango_modificar(elmultiplo));
@@ -44,39 +57,28 @@ function calcula_ip_final(elmultiplo){
 		}else{
 			console.log("%cFalta validar esto... ERROR","color:red");
 		}
-
 	}
-	console.log("%cIp final: "+(ip_red.toString()).replace(/,/g,"."),"color:green");
+	
+	imprimir("Ip final ",ip_red,"","green");
 }
 
 function que_rango_mascara(la_potencia) {
 	var cual=0000;
-
-	if (la_potencia==8) {
-		cual=8;
-	}
-	if (la_potencia<=7) {
-		cual=1;
-	}
-
-	if (la_potencia>8) {
-		cual=2;
-	}
-	var mascara="";
+	if (la_potencia==8) { cual=8; }
+	if (la_potencia<=7) { cual=1; }
+	if (la_potencia>8) { cual=2; }
 	switch (cual) {
-		
 	    case 1:
-		    console.log("%cMascara : 255.255.255."+(calcula_mascara(la_potencia).toString()),"color:purple");
+	    mascara="255.255.255."+(calcula_mascara(la_potencia).toString());
 	        break;
 	    case 2:
-
-	    	console.log("%cMascara : 255.255."+(calcula_mascara(la_potencia-8).toString()+".0"),"color:purple");
+	    	mascara="255.255."+(calcula_mascara(la_potencia).toString())+".0";
 	        break;
 	    case 8:
-	    	console.log("%cMascara : 255.255.255.0","color:purple");
+	    	mascara="255.255.255.0";
 	    	break;
 	    default: 
-	        console.log("%cMascara : Algo anda mal","color:red");
+	    	mascara="ERROR Mascara : Algo anda mal";
 	}
 }
 
@@ -127,4 +129,16 @@ document.onkeydown=function(evt){  // Para cuando se presiona ENTER
 	var keyCode = evt ? (evt.which ? evt.which : evt.keyCode) : event.keyCode;
 	if(keyCode == 13)
 		main();
+}
+
+function myFunction() {
+    var enrutamiento = document.forms[0];
+    var txt = "";
+    var i;
+    for (i = 0; i < enrutamiento.length; i++) {
+        if (enrutamiento[i].checked) {
+            txt = txt + enrutamiento[i].value + " ";
+        }
+    }
+    console.log(txt);
 }
